53d8251cc2f4b4d9d233edc8ffcaeb6ad645d153
Petrik <petrik@deheus.net>
2009-02-09 00:37:24 +0100

    Made redeirect sorting significant

--- lib/redirect.rb
+++ lib/redirect.rb
@@ -28,7 +28,8 @@ module Rack
     
     def call(env)
       req = Request.new(env)
-      @redirects.each_pair do |key, value|
+      @redirects.each do |pair|
+        key, value = pair
         if req.fullpath.match(key)
           return [301, {"Location" => value, "Content-Type" => "text/html"}, "Redirecting to: #{value}"]
         end
--- spec/rack_spec.rb
+++ spec/rack_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/helper'
 describe "Rack::Redirect" do
 
   it "should show 404 not found with if no redirect exists" do
-    @app = Rack::Redirect.new({})
+    @app = Rack::Redirect.new([])
     res = Rack::MockRequest.new(@app).get("/test")
 
     res.not_found?.should be_true
@@ -12,21 +12,21 @@ describe "Rack::Redirect" do
   end
   
   it "should redirect '/' if redirect exists" do
-    @app = Rack::Redirect.new({'/' => '/test'})
+    @app = Rack::Redirect.new([['/', '/test']])
     res = Rack::MockRequest.new(@app).get('/')
     res.headers.should == { 'Location' => '/test', 'Content-Type' => 'text/html' }
     res.body.should == 'Redirecting to: /test'
   end  
   
   it "should redirect if redirect exists" do
-    @app = Rack::Redirect.new({'/old/2008' => '/2008-new'})
+    @app = Rack::Redirect.new([['/old/2008', '/2008-new']])
     res = Rack::MockRequest.new(@app).get('/old/2008')
     res.headers.should == { 'Location' => '/2008-new', 'Content-Type' => 'text/html' }
     res.body.should == 'Redirecting to: /2008-new'
   end
 
   it "should redirect for a regular_expression" do
-    @app = Rack::Redirect.new({'^/old' => '/new'})
+    @app = Rack::Redirect.new([['^/old', '/new']])
     res = Rack::MockRequest.new(@app).get('/old/2008')
     res.headers.should == { 'Location' => '/new', 'Content-Type' => 'text/html' }
     res.body.should == 'Redirecting to: /new'
