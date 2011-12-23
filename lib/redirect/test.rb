6536c8efbf61ed1555c4725edf40643cc34ba714
Marek de Heus <marek@deheus.net>
2011-12-23 20:11:03 +0100

    Made test method less brittle

--- lib/redirect/test.rb
+++ lib/redirect/test.rb
@@ -5,7 +5,8 @@ class Test::Unit::TestCase
 
   def assert_redirects(from, to)
     res = Rack::MockRequest.new(APP[:redirect_app]).get(from)
-    assert_equal(res.headers, { 'Location' => to, 'Content-Type' => 'text/html' })
+    assert_equal to,          res.headers['Location'    ]
+    assert_equal 'text/html', res.headers['Content-Type']
   end
 
 end
